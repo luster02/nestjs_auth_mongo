82ce7528190292bc9f23dd6215593bc483384628
luster02 <bombolti_02@hotmail.com>
2020-11-13 16:15:29 -0600

    [ADD]: import modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,12 @@ import { AppService } from './app.service';
 import { IEnvironmentVariables } from '@config/configuration.interface'
 import { ConfigurationModule } from './configuration/configuration.module';
 import { DatabaseModule } from './database/database.module';
+import { UserModule } from './modules/user/user.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { GqlModule } from './gql/gql.module';
 
 @Module({
-  imports: [ConfigurationModule, DatabaseModule],
+  imports: [ConfigurationModule, DatabaseModule, UserModule, AuthModule, GqlModule],
   controllers: [AppController],
   providers: [AppService],
 })
