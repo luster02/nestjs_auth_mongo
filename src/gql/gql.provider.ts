6dc7ac3d73cde7a9ebe2b3c42479316a7ed4a06c
luster02 <bombolti_02@hotmail.com>
2020-11-13 16:47:06 -0600

    [ADD]: import modules

--- src/gql/gql.provider.ts
+++ src/gql/gql.provider.ts
@@ -1,10 +1,16 @@
 import { join } from "path";
 import { GraphQLModule } from "@nestjs/graphql";
+import { UserModule } from '@user/user.module'
+import { AuthModule } from '@auth/auth.module'
 
 export const GqlProvider = [
     GraphQLModule.forRoot({
         autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
         context: ({ req }) => ({ req }),
         installSubscriptionHandlers: true,
+        include: [
+            UserModule,
+            AuthModule,
+        ]
     })
 ]
