f99c6dc8f68c16ca80dc533e3ca10595bba9e0b5
luster02 <bombolti_02@hotmail.com>
2020-11-13 16:14:44 -0600

    [ADD]: jwt config key

--- src/configuration/configuration.interface.ts
+++ src/configuration/configuration.interface.ts
@@ -1,4 +1,5 @@
 export interface IEnvironmentVariables {
     PORT: number;
     MONGODB_URI: string;
+    JWT_SECRET: string;
 }
--- src/configuration/configuration.provider.ts
+++ src/configuration/configuration.provider.ts
@@ -1,4 +1,5 @@
 export default () => ({
     PORT: parseInt(process.env.PORT),
-    MONGODB_URI: process.env.MONGODB_URI
+    MONGODB_URI: process.env.MONGODB_URI,
+    JWT_SECRET: process.env.JWT_SECRET
 })
